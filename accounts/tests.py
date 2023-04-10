ec0838b18d8afc457c35d4a5df4c985c864406c3
Manh Hung DOAN <doanhung1710@gmail.com>
2023-04-10 21:54:49 +0200

    Ajouter les test pour l'app 'accounts'

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
+from .forms import CustomUserCreationForm
+from .views import SignupCreateView
 
 # Create your tests here.
 
@@ -31,3 +34,39 @@ class CustomUserTests(TestCase):
         self.assertTrue(super_user.is_active)
         self.assertTrue(super_user.is_staff)
         self.assertTrue(super_user.is_superuser)
+
+class HomepageTests(TestCase):
+
+    '''
+    La fonction "setUp" a été pré-construit. Elle va rechercher le nom d'url qui est coresspondant\
+    avec demande (ne peut pas changer de nom)
+    '''
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    '''
+    L1: Le test existe: pour vérifier si le code de HTTP status d'url dans la fonction "setUp" \
+        est égal 200, ce qui signifie qu'il existe.\
+    L2: Le test template: on peut comfimer que l'url utilise le template correct \
+    L3 + L4: Le test HTML: on peut vérifier que le "homepage" a le bon HTML + texte ou pas
+    '''
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, "S'inscrire")
+        self.assertNotContains(self.response, "Un element au hasard")
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    '''
+    Le test "resolve": vérifier la fonction "SignupCreateView" (dans accounts/views.py) a vraiment résolus un path d'URL donné \
+    et le test ici va checker si le nom de "view" est correspondant avec "SignupCreateView" 
+    '''
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignupCreateView.as_view().__name__)
+
